1569acee6da7516a9d3331f0a1f5075d10707f70
Elitrirudiyati <71365611+Elitrirudiyati@users.noreply.github.com>
2020-09-20 20:57:45 +0700

    Add files via upload

---
+++ tugas7.js
@@ -0,0 +1,9 @@
+var tinggiBadan= [160,165,155,150,170,175,180,158,167,185];
+	console.log("perulangan biasa, mengambil componen object");
+	for (var i=0; i<tinggiBadan.length;i++){
+	console.log(tinggiBadan [i])
+}
+	console.log("perulangan for of, x mengambil nilai dari array")
+	for(let x of tinggiBadan){
+		console.log(x);
+	}
